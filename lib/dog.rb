78f67190bf51481fb1851c0572c8b1a106870c28
Akilah Snowden <akilahdsnowden@gmail.com>
2022-06-09 18:22:19 -0400

    commit tree 899fa898a934124c5dd222391c0859a8fc3ebb01

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+
+
+class Dog
+# class body
+
+#instance method definition
+    def sit
+        puts "The Dog is sitting"
+    end
+    def bark
+     puts "Woof!"
+    end
+end 
+
+fido = Dog.new
+fido.bark
+
+snoopy = Dog.new
+snoopy.bark
